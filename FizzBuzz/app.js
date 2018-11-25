5afc3070cd3be51220fbd7e2a8745a3f39269fdf
LeslieSaVa <leslie.sabattin@usach.cl>
2018-11-24 21:59:45 -0300

    subiendo carpeta FizzBuzz

---
+++ FizzBuzz/app.js
@@ -0,0 +1,43 @@
+// primero vamos a imprimir los numeros del 1 al 100
+//for( var number = 1; number <= 100; number++){
+  //if(number % 3 === 0 && number % 5 ===0){
+    //console.log("FizzBuzz");
+  //}else if (number % 3 === 0){
+    //console.log("Fizz");
+  //} else if (number % 5 === 0 ){
+    //console.log("Buzz")
+// un else para que se ejecute en todos los demas casos divisible en 3 o 5
+  //} else {
+    //console.log(number);
+  //}
+//}
+
+//const areOdd = (n, m) => {
+  //let estrue;
+
+//if (n % 2 === 0 &&  m % 2 ===0){
+  //estrue = true;
+//} else {
+  //estrue=false;
+//}
+//return estrue;
+//}
+
+//const evenOrOdd = (n) => {
+  //let estrue;
+  //if (n % 2 === 0){
+  //estrue = 0;
+  //}else{
+  //estrue = 1;
+  //}
+  //return estrue;
+//}
+
+const countVowels = (str) => {
+  let counter = 0;
+  let vocales =[ "a", "e", "i", "o", "u"];
+  for 
+
+
+
+};
